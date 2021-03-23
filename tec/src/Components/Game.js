45594d1f4aff3ddf3663b644328b5d6e2fa8d0a2
Hiba Tamimi <40838567+hibtmimi@users.noreply.github.com>
2021-03-23 22:23:34 +0200

    functinaity

--- tec/src/Components/Game.js
+++ tec/src/Components/Game.js
@@ -4,24 +4,53 @@ import { calculateWinner } from '../helpers'
 
 const Game = () => {
 
+    const styles = {
+        width: '200px',
+        margin: '20px auto',
+    };
+
     const [board, setBoard] = useState(Array(9).fill(null));
     
     const [xIsNext, setXisNext] = useState(true);
 
     const winner = calculateWinner(board)
 
-    const handleClick = () => {
+    // const marks = [mark, setMrks] = useState;
+
+    // const mark = () => {
+    //     <label onClick={() => }> </label>
+    // }
+
+    const handleClick = i => {
+
+        const boredCopy = [...board];
+        if (winner || boredCopy[i]) return 'no one ';
+        boredCopy[i] = xIsNext ? 'X' : "O";
+        setBoard(boredCopy);
+        setXisNext(!xIsNext);
+        }
         
-    }
+    
     const jumpTo = () => {
         
     }
     
-    const renderMoves = () => {
-        
-    }
+    const renderMoves = () => (
+        <button onClick={() => setBoard(Array(9).fill(null))}>
+            Start Game
+        </button>
+    )
     return (
-        <Board squares={board} onClick={handleClick}/>
+        
+        <div>
+        <Board squares={board} onClick={handleClick} />
+        <div style={styles}>
+                <p>{winner ? 'congrate winner is: '  + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+                {renderMoves()}
+        </div>
+        
+        </div>
+    
     )
 }
 export default Game;
